f48acc382f062fb366e885e7f683e1a2e4ded3b5
Wobin <nathanielmusa3@gmail.com>
2021-05-26 00:27:14 +0100

    added a class that display profile details

---
+++ classes-written-in-java-script/profile.js
@@ -0,0 +1,17 @@
+function profile() {
+    let name, phone, email, address, occupation;
+
+    name = prompt("please enter your name ");
+    phone = prompt("please enter your phone number ");
+    email = prompt("please enter your email address");
+    address = prompt("please enter your addres ");
+    occupation = prompt("please enter your occupation");
+
+    console.log(name);
+    console.log(phone);
+    console.log(email);
+    console.log(address);
+    console.log(occupation);
+}
+
+profile();

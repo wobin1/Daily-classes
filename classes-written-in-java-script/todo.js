ebcbeffabac50345db79eb9841c7f4ad48e849af
Wobin <nathanielmusa3@gmail.com>
2021-05-26 00:17:07 +0100

    added a todo list class

--- classes-written-in-java-script/todo.js
+++ classes-written-in-java-script/todo.js
@@ -1,18 +1,22 @@
 function todo() {
     let choice1, choice2;
-    
+  
     // function for adding task
     function addTask() {
-        var task1 = prompt("add task");
-        var task2 = prompt("add task");
-        var task3 = prompt("add task");
-        var task4 = prompt("add task");
-        var task5 = prompt("add task");
-        var task6 = prompt("add task");
-        var task7 = prompt("add task");
-        var task8 = prompt("add task");
-        var task9 = prompt("add task");
-        var task10 = prompt("add task");
+        var task = [""]
+        var taskAdd = prompt("add task");
+        task.push(taskAdd);
+
+        var con = prompt("do you want to add another task; yes or No");
+
+        if(con == "yes") {
+            addTask();
+        } else {
+            for(let i in task){
+                console.log(task[i]);
+            }
+        }
+        
     }
 
     // message that is asking of what you want to do
@@ -25,16 +29,10 @@ function todo() {
         addTask();
     }else if(choice == choice2) {
         console.log("thank you for working with us");
-        console.log(task1);
-        console.log(task2);
-        console.log(task3);
-        console.log(task4);
-        console.log(task5);
-        console.log(task6);
-        console.log(task7);
-        console.log(task8);
-        console.log(task9);
-        console.log(task10);
+        for(let i in task){
+            console.log(task[i]);
+        }
+       
     }else {
         console.log("you have made an invalid choice");
     }
